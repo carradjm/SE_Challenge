34e42c331f77e5a6d643a699ed57a2e964aa2a04
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-10-20 10:48:30 -0400

    Removes need for API key, adds comments

--- street_easy.rb
+++ street_easy.rb
@@ -1,26 +1,28 @@
-# I didn't feel the need to add any error rescuing.
+# I didn't feel the need to add any error rescuing. The way the script is
+# set up, all of the logic is abstracted from the user, so as long as the user
+# runs the script, there will be no errors.
 # I also tried to keep the number of instance variables as low as possible.
 # The use of Addressable::URI makes it easy to search for other types
-#   of listings / use other criteria if we ever wanted to change the script.
+# of listings / use other criteria if we ever wanted to change the script.
 
 require 'rest_client'
 require 'addressable/uri'
 require 'json'
 
 class StreetEasy
-  
-  attr_reader :listing_type, :area, :api_key
 
-  def initialize(listing_type, area, api_key)
+  def initialize(listing_type, area)
     @listing_type = listing_type
     @area = area
-    @api_key = api_key
   end
   
   def build_output
     output = {@listing_type =>[]}
     
     listings = JSON.parse(self.get_api_data)
+    
+    # Goes through each listing returned from the API call, removes the information
+    # we need, puts that info in a new hash, and then pushes that hash into our array.
     
     listings['listings'].each do |listing|
       output[@listing_type].push(
@@ -38,32 +40,41 @@ class StreetEasy
   
   def get_api_data
     url = self.build_url
+    
+    #RestClient makes it super easy to make API requests.
     RestClient.get url
   end
   
   def build_url
     # Can change any of these parameters very quickly; makes it easy to get different data
+    
+    # The defeault 'order' for an API search is 'price DESC' so I left that out of the 'query_values'.
+    # Same with 'limit'.  It already defaults to the top 20.  If we wished to change these query values we could
+    # easily add them in.
+    
     @url = Addressable::URI.new(
       :scheme => "http",
       :host => "streeteasy.com",
       :path => "nyc/api/#{@listing_type}/search",
       :query_values => {
         :criteria => "area:#{@area}",
-        :key => @api_key,
-        :format => 'json',
+        :format => 'json'
         }
     ).to_s
   end
   
 end
 
-puts "Please enter your API key for StreetEasy.com: "
+# Originally I had the user enter their API key here when the script ran.
+# However, I realized that in order to get search API data from StreetEasy,
+# you don't need an API key! So I removed that logic from the script.
 
-api_key = gets.chomp.to_s
+sales = StreetEasy.new('sales', 'soho').build_output
 
-sales = StreetEasy.new('sales', 'soho', api_key).build_output
+rentals = StreetEasy.new('rentals', 'soho').build_output
 
-rentals = StreetEasy.new('rentals', 'soho', api_key).build_output
+# Will write the returned output from the api to a file named
+# 'output.json' on the desktop
 
 File.open("output.json","w") do |file|
   file.write(sales.merge!(rentals).to_json)
